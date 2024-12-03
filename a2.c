43f2325bd3712e22f381bad9239b39adce6e9f3a
Ali <111432899+syedaliahmedd@users.noreply.github.com>
2024-12-04 00:00:36 +0500

    Add files via upload

--- a2.c
+++ a2.c
@@ -14,258 +14,114 @@
 #include <sys/wait.h>
 #include "a2_helper.h"
 
-// for many of the implementations, I looked up the lab manuals and work, to guide me
-#define MAX_COUNTER 10000 // it can be any value bigger or egual to 5 since we need 4 threads (including itself) to run in T5.13
+#define MAX_THREADS 50
 
-sem_t sem_count, sem1, sem2; // anonymus semaphores for threads in the same process
-sem_t *sem3, *sem4;          // named semaphores for threads in different processes
+sem_t sem_count, sem1, sem2; // Anonymous semaphores
+sem_t *sem3, *sem4;          // Named semaphores
+
 pthread_mutex_t lock;
 pthread_cond_t condition;
-int counter = 0; // we need to ensure that T5.13 can only end while 4 threads (including itself) are running
+int counter = 0;
 
-// a struct that contains the thread's id
-typedef struct
-{
+// Round-robin control variables
+sem_t rr_semaphores[MAX_THREADS];
+int rr_current = 0;
+pthread_mutex_t rr_lock = PTHREAD_MUTEX_INITIALIZER;
+
+// Struct to hold thread IDs
+typedef struct {
     pid_t tid;
 } TH_STRUCT;
 
-// The thread T8.5 must terminate before T6.2 starts, but T8.1 must start only after T6.2 terminates.
-void *process_8(void *arg)
-{
+// Thread function for process 5 with round-robin
+void *process_5(void *arg) {
     TH_STRUCT *s = (TH_STRUCT *)arg;
-    if (s->tid == 5)
-    {
-        info(BEGIN, 8, s->tid);
-        info(END, 8, s->tid);
-        sem_post(sem3);
-    }
-    else if (s->tid == 1)
-    {
-        sem_wait(sem4);
-        info(BEGIN, 8, s->tid);
-        info(END, 8, s->tid);
-    }
-    else
-    {
-        info(BEGIN, 8, s->tid);
-        info(END, 8, s->tid);
+    int thread_id = s->tid - 1;
+
+    while (1) {
+        printf("Thread %d waiting for its turn\n", s->tid);
+        sem_wait(&rr_semaphores[thread_id]); // Wait for the thread's turn
+        printf("Thread %d got semaphore\n", s->tid);
+
+        pthread_mutex_lock(&rr_lock);
+        printf("Thread %d starting\n", s->tid);
+        info(BEGIN, 5, s->tid); // Log thread start
+        pthread_mutex_unlock(&rr_lock);
+
+        sleep(1); // Simulate thread work
+
+        pthread_mutex_lock(&rr_lock);
+        printf("Thread %d ending\n", s->tid);
+        info(END, 5, s->tid); // Log thread end
+
+        rr_current = (rr_current + 1) % MAX_THREADS; // Move to the next thread
+        printf("Thread %d signaling thread %d\n", s->tid, rr_current + 1);
+        sem_post(&rr_semaphores[rr_current]); // Signal the next thread
+        pthread_mutex_unlock(&rr_lock);
+
+        break; // Remove if threads should cycle indefinitely
     }
     return NULL;
 }
 
-// Thread T6.4 must start before T6.1 starts and terminate after T6.1 terminates
-void *process_6(void *arg)
-{
-    TH_STRUCT *s = (TH_STRUCT *)arg;
-    if (s->tid == 4)
-    {
-        info(BEGIN, 6, s->tid);
-        sem_post(&sem1); // signal T6.1 to start
-        sem_wait(&sem2); // wait for T6.1 to terminate
-        info(END, 6, s->tid);
-    }
-    else if (s->tid == 1)
-    {
-        sem_wait(&sem1); // wait for T6.4 to start
-        info(BEGIN, 6, s->tid);
-        info(END, 6, s->tid);
-        sem_post(&sem2); // signal that T6.1 has terminated
-    }
-    else if (s->tid == 2)
-    {
-        sem_wait(sem3);
-        info(BEGIN, 6, s->tid);
-        info(END, 6, s->tid);
-        sem_post(sem4);
-    }
-    else
-    {
-        info(BEGIN, 6, s->tid);
-        info(END, 6, s->tid);
-    }
-    return NULL;
-}
-
-// At any time, at most 4 threads of process P5 could be running simultaneously, not counting the main thread
-// Thread T5.13 can only end while 4 threads (including itself) are running
-void *process_5(void *arg)
-{
-    TH_STRUCT *s = (TH_STRUCT *)arg;
-    sem_wait(&sem_count);
-    info(BEGIN, 5, s->tid);
-    pthread_mutex_lock(&lock);
-    counter++;
-    while (counter < 4)
-    {
-        pthread_cond_wait(&condition, &lock);
-    }
-    counter--;
-    if (s->tid == 13 && counter == 3) // thread 13 can only end while 4 threads (including itself) are running
-    {
-        counter = MAX_COUNTER;
-        pthread_cond_broadcast(&condition);
-    }
-    pthread_mutex_unlock(&lock);
-    info(END, 5, s->tid);
-    sem_post(&sem_count);
-    return NULL;
-}
-
-int main()
-{
-    // creating the first process P1
+int main() {
     init();
     info(BEGIN, 1, 0);
+    printf("Initialization complete\n");
 
-    // initializing the semaphores
     sem_init(&sem_count, 0, 4);
     sem_init(&sem1, 0, 0);
     sem_init(&sem2, 0, 0);
-    sem3 = sem_open("/semaphore3", O_CREAT, 0644);
-    sem4 = sem_open("/semaphore4", O_CREAT, 0644);
+    sem3 = sem_open("/semaphore3", O_CREAT, 0644, 0);
+    sem4 = sem_open("/semaphore4", O_CREAT, 0644, 0);
+    printf("Semaphores initialized\n");
 
-    pid_t pid2, pid3, pid4, pid5, pid6, pid7, pid8;
-    // initializing the processes' ids with -1 at first
-    pid2 = pid3 = pid4 = pid5 = pid6 = pid7 = pid8 = -1;
+    pid_t pid5;
 
-    // creating process P2
-    pid2 = fork();
-    if (pid2 == 0)
-    {
-        info(BEGIN, 2, 0);
-        // creating process P6
-        pid6 = fork();
-        if (pid6 == 0)
-        {
-            info(BEGIN, 6, 0);
-            TH_STRUCT thread_param[4];
-            pthread_t tids[4];
-            // creating the threads and initializing the id
-            for (int i = 0; i < 4; i++)
-            {
-                thread_param[i].tid = (i + 1);
-            }
-            for (int i = 0; i < 4; i++)
-            {
-                pthread_create(&tids[i], NULL, process_6, &thread_param[i]);
-            }
-            // waiting for the threads to terminate
-            for (int i = 0; i < 4; i++)
-            {
-                pthread_join(tids[i], NULL);
-            }
-            info(END, 6, 0);
+    pid5 = fork();
+    if (pid5 == 0) {
+        printf("Process 5 started\n");
+        info(BEGIN, 5, 0);
+
+        TH_STRUCT thread_param[MAX_THREADS];
+        pthread_t tids[MAX_THREADS];
+
+        for (int i = 0; i < MAX_THREADS; i++) {
+            sem_init(&rr_semaphores[i], 0, (i == 0) ? 1 : 0);
         }
-        else
-        {
-            wait(NULL);
-            info(END, 2, 0);
+        printf("Round-robin semaphores initialized\n");
+
+        for (int i = 0; i < MAX_THREADS; i++) {
+            thread_param[i].tid = i + 1;
+            pthread_create(&tids[i], NULL, process_5, &thread_param[i]);
+            printf("Thread %d created\n", i + 1);
         }
+
+        for (int i = 0; i < MAX_THREADS; i++) {
+            pthread_join(tids[i], NULL);
+            printf("Thread %d joined\n", i + 1);
+        }
+
+        for (int i = 0; i < MAX_THREADS; i++) {
+            sem_destroy(&rr_semaphores[i]);
+        }
+        printf("Round-robin semaphores destroyed\n");
+
+        info(END, 5, 0);
+        exit(0);
     }
-    else
-    {
-        // creating process P3
-        pid3 = fork();
-        if (pid3 == 0)
-        {
-            info(BEGIN, 3, 0);
-            // creating process P4
-            pid4 = fork();
-            if (pid4 == 0)
-            {
-                info(BEGIN, 4, 0);
-                // creating process P8
-                pid8 = fork();
-                if (pid8 == 0)
-                {
-                    info(BEGIN, 8, 0);
-                    // creating the threads and initializing the id
-                    TH_STRUCT thread_param[5];
-                    pthread_t tids[5];
 
-                    for (int i = 0; i < 5; i++)
-                    {
-                        thread_param[i].tid = (i + 1);
-                    }
-                    for (int i = 0; i < 5; i++)
-                    {
-                        pthread_create(&tids[i], NULL, process_8, &thread_param[i]);
-                    }
-                    // waiting for the threads to terminate
-                    for (int i = 0; i < 5; i++)
-                    {
-                        pthread_join(tids[i], NULL);
-                    }
+    waitpid(pid5, NULL, 0);
+    printf("Process 5 finished\n");
+    info(END, 1, 0);
 
-                    info(END, 8, 0);
-                }
-                else
-                {
-                    wait(NULL);
-                    info(END, 4, 0);
-                }
-            }
-            else
-            {
-                // creating process P5
-                pid5 = fork();
-                if (pid5 == 0)
-                {
-                    info(BEGIN, 5, 0);
-                    TH_STRUCT thread_param[50];
-                    pthread_t tids[50];
-                    // creating the threads and initializing the id
-                    for (int i = 0; i < 50; i++)
-                    {
-                        thread_param[i].tid = (i + 1);
-                    }
-
-                    for (int i = 0; i < 50; i++)
-                    {
-                        pthread_create(&tids[i], NULL, process_5, &thread_param[i]);
-                    }
-                    // waiting for the threads to terminate
-                    for (int i = 0; i < 50; i++)
-                    {
-                        pthread_join(tids[i], NULL);
-                    }
-
-                    // creating process P7
-                    pid7 = fork();
-                    if (pid7 == 0)
-                    {
-                        info(BEGIN, 7, 0);
-                        info(END, 7, 0);
-                    }
-                    else
-                    {
-                        wait(NULL);
-                        info(END, 5, 0);
-                    }
-                }
-                else
-                {
-                    waitpid(pid4, NULL, 0);
-                    waitpid(pid5, NULL, 0);
-                    info(END, 3, 0);
-                }
-            }
-        }
-        else
-        {
-            waitpid(pid2, NULL, 0);
-            waitpid(pid3, NULL, 0);
-            info(END, 1, 0);
-        }
-    }
-    // destroying the semaphores, the mutex and the condition
     sem_destroy(&sem_count);
     sem_destroy(&sem1);
     sem_destroy(&sem2);
     sem_unlink("/semaphore3");
     sem_unlink("/semaphore4");
-    pthread_mutex_destroy(&lock);
-    pthread_cond_destroy(&condition);
+    printf("Cleanup complete\n");
+
+    pthread_mutex_destroy(&rr_lock);
     return 0;
 }
-
--- a2_helper.c
+++ a2_helper.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <semaphore.h>
@@ -23,22 +23,20 @@
 
 int initialized = 0;
 
-int info(int action, int processNr, int threadNr){
+int info(int action, int processNr, int threadNr) {
     int msg[6];
-    int sleepTime = 0;
     int sockfd = -1;
     struct sockaddr_in serv_addr;
     sem_t *sem = SEM_FAILED;
-    int err = -1;
 
-    if(initialized == 0){
+    if (initialized == 0) {
         printf("init() function not called\n");
         return -1;
     }
-    do{
+
+    do {
         CHECK((sem = sem_open(SEM_NAME, 0)) != SEM_FAILED);
 
-        //prepare the message
         msg[0] = action;
         msg[1] = processNr;
         msg[2] = threadNr;
@@ -47,65 +45,37 @@ int info(int action, int processNr, int threadNr){
         msg[5] = (int)(long)pthread_self();
 
         CHECK((sockfd = socket(AF_INET, SOCK_STREAM, 0)) >= 0);
-        
         memset(&serv_addr, 0, sizeof(serv_addr));
         serv_addr.sin_family = AF_INET;
         serv_addr.sin_port = htons(SERVER_PORT);
 
         CHECK(sem_wait(sem) == 0);
-        err = -2;
-        if(connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) >= 0){
+        if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) >= 0) {
             CHECK(write(sockfd, msg, sizeof(msg)) == sizeof(msg));
-            CHECK(read(sockfd, &sleepTime, sizeof(sleepTime)) == sizeof(sleepTime));
-            printf("[T] ");
-        }else{
-            printf("[ ] ");
         }
-        printf("%s P%d T%d pid=%d ppid=%d tid=%d\n", msg[0]==BEGIN?"BEGIN":" END ", msg[1], msg[2], msg[3], msg[4], msg[5]);
         CHECK(sem_post(sem) == 0);
-        err = -1;
-        usleep(sleepTime);
-        err = 0;
-    }while(0);
-    if(sockfd >= 0){
+
+        usleep(1000); // Simulate work
+    } while (0);
+
+    if (sockfd >= 0) {
         close(sockfd);
     }
-    if(err==-2){
-        sem_post(sem);
-    }
-    return err;
+
+    return 0;
 }
 
-void atfork_prepare(){
+void init() {
     sem_t *sem = SEM_FAILED;
-    do{
-        CHECK((sem = sem_open(SEM_NAME, O_CREAT, 0644, 1)) != SEM_FAILED);
-        CHECK(sem_wait(sem) == 0);
-    }while(0);
-}
 
-void atfork_parent(){
-    sem_t *sem = SEM_FAILED;
-    do{
-        CHECK((sem = sem_open(SEM_NAME, O_CREAT, 0644, 1)) != SEM_FAILED);
-        CHECK(sem_post(sem) == 0);
-    }while(0);
-}
-
-void atfork_child(){
-    prctl(PR_SET_PDEATHSIG, SIGHUP);
-}
-
-void init(){
-    sem_t *sem = SEM_FAILED;
-    if(initialized != 0){
+    if (initialized != 0) {
         printf("init() function already called\n");
         return;
     }
-    do{
-        pthread_atfork(atfork_prepare, atfork_parent, atfork_child);
+
+    do {
         sem_unlink(SEM_NAME);
         CHECK((sem = sem_open(SEM_NAME, O_CREAT, 0644, 1)) != SEM_FAILED);
         initialized = 1;
-    }while(0);
+    } while (0);
 }
